aeeca1b07eab04977e120710cd38d65c66414872
nagammanikanta <nagammanikanta@gmail.com>
2023-12-17 14:18:10 +0530

    init

--- .terraform/gc.tf/gcs.tf
+++ .terraform/gc.tf/gcs.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "static-bucket" {
   name          = "static-bucket"
-  location      = "us-central1-b"
+  location      = "US"
   force_destroy = true
 
   uniform_bucket_level_access = true
