06aa1ce32168b0f2870f619d5bbdf1ebc5ffb0ff
nagammanikanta <nagammanikanta@gmail.com>
2023-12-21 09:50:41 +0530

    init

--- cloudnat.tf
+++ cloudnat.tf
@@ -31,30 +31,3 @@ resource "google_network_connectivity_spoke" "spoke-tf" {
   }
 }
 
-resource "google_compute_router_nat" "nat-tf" {
-  provider                            = google-beta
-  project =  var.project_id
-  
-  name                                = var.nat-tf
-  router                              = google_compute_router.router-tf.name
-  region                              = var.region
-  source_subnetwork_ip_ranges_to_nat  = "LIST_OF_SUBNETWORKS"
-  enable_dynamic_port_allocation      = false
-  enable_endpoint_independent_mapping = false
-  min_ports_per_vm                    = 32
-  type                                = "PRIVATE"
-  subnetwork {
-    name                    = google_compute_subnetwork.subnet-tf.id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-  }
-  rules {
-    rule_number = 100
-    description = "rule for private nat"
-    match       = "nexthop.hub == \"//networkconnectivity.googleapis.com/projects/leafy-summer-405104/locations/global/hubs/my-hub-tf\""
-    action {
-      source_nat_active_ranges = [
-        google_compute_subnetwork.subnet-tf.self_link
-      ]
-    }
-  }
-}
